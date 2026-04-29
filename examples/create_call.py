d679995a26b1e3321ff8faea011a632c0064afa9
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 18:11:59 -0300

    Expand Python examples for auth, OTP, calls and WhatsApp

---
+++ examples/create_call.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from nvoip import NvoipClient
+
+
+client = NvoipClient(
+    base_url=os.getenv("NVOIP_BASE_URL", "https://api.nvoip.com.br/v2"),
+    oauth_basic_auth=os.getenv("NVOIP_OAUTH_BASIC_AUTH"),
+    oauth_client_id=os.getenv("NVOIP_OAUTH_CLIENT_ID"),
+    oauth_client_secret=os.getenv("NVOIP_OAUTH_CLIENT_SECRET"),
+)
+oauth = client.create_access_token(
+    numbersip=os.environ["NVOIP_NUMBERSIP"],
+    user_token=os.environ["NVOIP_USER_TOKEN"],
+)
+
+response = client.create_call(
+    caller=os.environ["NVOIP_CALLER"],
+    called=os.getenv("NVOIP_TARGET_NUMBER", "11999999999"),
+    access_token=oauth["access_token"],
+)
+
+print(json.dumps(response, indent=2, ensure_ascii=False))
